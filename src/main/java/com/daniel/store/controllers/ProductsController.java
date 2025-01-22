04184009de39ceff60b3cada23d4e504fe317741
Daniel <danieloluwasegun1000@gmail.com>
2025-01-22 02:57:11 +0100

    edit-product-page

--- src/main/java/com/daniel/store/controllers/ProductsController.java
+++ src/main/java/com/daniel/store/controllers/ProductsController.java
@@ -1,16 +1,26 @@
 package com.daniel.store.controllers;
 
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.daniel.store.models.Product;
+import com.daniel.store.models.ProductDto;
 import com.daniel.store.services.ProductsRepository;
 
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/products")
@@ -19,13 +29,88 @@ public class ProductsController {
     @Autowired
     private ProductsRepository repository;
 
-    @GetMapping({"", "/"})
-public String showProductList(Model model) {
-    List<Product> products = repository.findAll();
-    model.addAttribute("products", products);
-    return "product";
-}
+    @GetMapping({ "", "/" })
+    public String showProductList(Model model) {
+        List<Product> products = repository.findAll();
+        model.addAttribute("products", products);
+        return "product";
+    }
 
+    @GetMapping("/create")
+    public String showCreatePage(Model model) {
+        ProductDto productDto = new ProductDto();
+        model.addAttribute("productDto", productDto);
+        return "createProduct";
+    }
 
+    @PostMapping("/create")
+    public String createProduct(
+            @Valid @ModelAttribute ProductDto productDto,
+            BindingResult result) {
+        if (productDto.getImageFile().isEmpty()) {
+            result.addError(new FieldError("productDto", "imageFile", "The image file is required"));
+        }
+
+        if (result.hasErrors()) {
+            return "CreateProduct";
+        }
+
+        // save image file
+        MultipartFile image = productDto.getImageFile();
+        Date createdAt = new Date();
+        String storageFileName = createdAt.getTime() + "_" + image.getOriginalFilename();
+
+        try {
+            String uploadDir = "public/images/";
+            Path uploadPath = Paths.get(uploadDir);
+
+            if (!Files.exists(uploadPath)) {
+                Files.createDirectories(uploadPath);
+            }
+
+            try (InputStream inputStream = image.getInputStream()) {
+                Files.copy(inputStream, Paths.get(uploadDir + storageFileName), StandardCopyOption.REPLACE_EXISTING);
+            }
+
+        } catch (Exception ex) {
+            System.out.println("Exception: " + ex.getMessage());
+        }
+
+        Product product = new Product();
+        product.setName(productDto.getName());
+        product.setBrand(productDto.getBrand());
+        product.setCategory(productDto.getCategory());
+        product.setPrice(productDto.getPrice());
+        product.setDescription(productDto.getDescription());
+        product.setCreatedAt(createdAt);
+        product.setImageFileName(storageFileName);
+        repository.save(product);
+
+        return "redirect:/products";
+    }
+
+    @GetMapping("/edit")
+    public String showEditPage(
+            Model model,
+            @RequestParam int id) {
+        try {
+            Product product = repository.findById(id).get();
+            model.addAttribute("product", product);
+
+            ProductDto productDto = new ProductDto();
+            productDto.setName(product.getName());
+            productDto.setBrand(product.getBrand());
+            productDto.setCategory(product.getCategory());
+            productDto.setPrice(product.getPrice());
+            productDto.setDescription(product.getDescription());
+
+            model.addAttribute("productDto", productDto);
+        } catch (Exception ex) {
+            System.out.println("Exception: " + ex.getMessage());
+            return "redirect:/products";
+        }
+
+        return "editProduct";
+    }
 
 }
